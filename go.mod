fde009dc35c0158a1c0817c80be9ae2b5b26db88
Thierry Grangeray <ext.thierry.grangeray@sncf.fr>
2022-03-02 18:11:59 +0100

    webview

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/tgrangeray/ViewTest
 
 go 1.17
 
-require github.com/karalabe/xgo v0.0.0-20191115072854-c5ccff8648a7 // indirect
+require (
+	github.com/karalabe/xgo v0.0.0-20191115072854-c5ccff8648a7 // indirect
+	github.com/webview/webview v0.0.0-20220228175528-9bda02f8085c
+)
--- main.go
+++ main.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import "github.com/webview/webview"
 
 func main() {
-	fmt.Println("hello world")
+	debug := true
+	w := webview.New(debug)
+	defer w.Destroy()
+	w.SetTitle("Minimal webview example")
+	w.SetSize(800, 600, webview.HintNone)
+	w.Navigate("https://en.m.wikipedia.org/wiki/Main_Page")
+	w.Run()
 }
